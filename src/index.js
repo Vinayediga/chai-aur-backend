3e7e46a258e7e6c4613fbdc640eb148940d5f899
vinay kumar Ediga <edigaravinaykumar@gmail.com>
2024-09-07 09:47:05 +0530

    setup

---
+++ src/index.js
@@ -0,0 +1,67 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv'
+
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from 'express';
+const app = express()
+
+{
+    async()=>{
+            
+        try {
+            
+           let connectionInstance = await mongoose.connection(`${process.env.MONGODB_URI}/${DB_NAME}`)
+           app.on("errror",(error) => {
+                console.log("Errr:",error);
+                throw error
+           })
+
+           app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on port ${process.env.PORT}`);
+            
+           })
+
+        } catch (error) {
+            console.log(error);
+            throw err
+            
+        }
+    
+
+
+
+}} {}
+*/
